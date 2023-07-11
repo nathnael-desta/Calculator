9b4c6588a4956c06b2d1c84f819842d4892c7420
Nathnael Desta <destanathnaelataro@gmail.com>
2023-07-11 16:24:40 +0300

    add html and css with query selectors

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(x, y) {
+    return x + y;
+}
+function subtract(x, y) {
+    return x - y;
+}
+function multiply(x, y) {
+    return x * y;
+}
+function devide(x, y) {
+    return x / y;
+}
+function remainder(x, y) {
+    return x % y;
+}
+
+const zero = document.querySelector('.zero');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const ac = document.querySelector('.ac');
+const sign = document.querySelector('.sign');
+const remain = document.querySelector('.remainder');
+const division = document.querySelector('.devide');
+const multiplication = document.querySelector('.multiply');
+const addition = document.querySelector('.plus');
+const subtraction = document.querySelector('.minus');
+const reset = document.querySelector('.reset');
+const equal = document.querySelector('.equals');
+const display = document.querySelector('.display');
+
+const isEqual = false;
+const num1 = null;
+const num2 = null;
+
+
